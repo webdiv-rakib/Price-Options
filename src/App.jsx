a242f845ac3cf9178c8f30a3a9cf4b0204953f70
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-03-02 03:45:02 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl'>Vite + React</h1>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
