5627401cd8332886f48ce9c0507b24b180369802
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-03-02 04:44:43 +0600

    working on new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
 
 function App() {
 
   return (
     <>
+      <DaisyNav></DaisyNav>
       <h1 className='text-2xl'>Vite + React</h1>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-              quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
     </>
   )
 }
