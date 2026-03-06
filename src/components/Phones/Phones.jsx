e111fae353ee15ef84e9bd15ccddec443d0a927f
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-03-07 05:09:13 +0600

    Explore React Awesome components

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Audio } from 'react-loader-spinner'
 const Phones = () => {
-    const [phones, setPhones] = useState([])
+    const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         // using fetch method
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -21,10 +23,22 @@ const Phones = () => {
                 })
                 console.log(phonesWithFakeData)
                 setPhones(phonesWithFakeData)
+                setLoading(false);
             })
     }, [])
     return (
         <div>
+            {
+                loading && <Audio
+                    height={80}
+                    width={80}
+                    radius={9}
+                    color="green"
+                    ariaLabel="audio-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                />
+            }
             <h2 className="text-2xl">Phones:{phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
                 <XAxis></XAxis>
